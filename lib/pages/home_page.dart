a860c3444aee2ed652852f26a021fb885da68e42
Pruthvi Patel <71627511+PatelPruthvi@users.noreply.github.com>
2022-04-24 15:34:10 +0530

    Day 5 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,9 +3,16 @@
 import 'package:codepur/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String name = "";
+  bool changedButt = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +31,7 @@ class HomePage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             Text(
-              'Welcome',
+              'Welcome $name',
               style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
@@ -38,6 +45,10 @@ class HomePage extends StatelessWidget {
                         labelText: 'E-MAIL',
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(60))),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   SizedBox(height: 20),
                   TextFormField(
@@ -49,18 +60,50 @@ class HomePage extends StatelessWidget {
                             borderRadius: BorderRadius.circular(60))),
                   ),
                   SizedBox(height: 40),
-                  ElevatedButton(
-                    onPressed: () {
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changedButt = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushReplacementNamed(
                           context, MyRoutes.loginRoute);
                     },
-                    child: Text(
-                      'SIGN-IN',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changedButt ? 50 : 150,
+                      height: 40,
+                      child: Center(
+                          child: changedButt
+                              ? Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : Text(
+                                  'Log-In',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                )),
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(changedButt ? 50 : 8),
+                        color: Colors.deepPurple,
+                      ),
                     ),
-                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
                   )
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     Navigator.pushReplacementNamed(context, MyRoutes.loginRoute);
+                  //   },
+                  //   child: Text(
+                  //     'SIGN-IN',
+                  //     style:
+                  //         TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  //   ),
+                  //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  // )
                 ],
               ),
             )
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,9 +2,14 @@
 
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
