16458a5097c1e42317db3e0a003506998a077fdd
Pruthvi Patel <71627511+PatelPruthvi@users.noreply.github.com>
2022-05-20 15:01:06 +0530

    day17 comp

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class cartPage extends StatelessWidget {
@@ -18,6 +19,63 @@ class cartPage extends StatelessWidget {
           ),
         ),
       ),
+      // ignore: prefer_const_literals_to_create_immutables
+      body: Column(children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: _cartList(),
+          ),
+        ),
+        Divider(),
+        _cartTotal()
+      ]),
     );
+  }
+}
+
+class _cartTotal extends StatelessWidget {
+  const _cartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        Text(
+          '\$9999',
+          style: TextStyle(fontSize: 26, color: Theme.of(context).accentColor),
+        ),
+        SizedBox(width: 20),
+        ElevatedButton(
+            style: ButtonStyle(
+                shape: MaterialStateProperty.all(StadiumBorder()),
+                backgroundColor:
+                    MaterialStateProperty.all(Theme.of(context).buttonColor)),
+            onPressed: () {},
+            child: Text('Buy')),
+      ]),
+    );
+  }
+}
+
+class _cartList extends StatefulWidget {
+  const _cartList({Key? key}) : super(key: key);
+
+  @override
+  State<_cartList> createState() => __cartListState();
+}
+
+class __cartListState extends State<_cartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) => ListTile(
+              leading: Icon(Icons.done),
+              trailing: IconButton(
+                  icon: Icon(Icons.remove_circle_outline), onPressed: () {}),
+              title: Text('item1'),
+            ));
   }
 }
